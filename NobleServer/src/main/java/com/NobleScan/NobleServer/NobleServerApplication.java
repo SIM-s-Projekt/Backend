a637d9ec4c69ee19e8267d3ae99308cd42885a8d
LevelWard <noah.jonsson@gmail.com>
2025-09-30 09:42:03 +0200

    Spring boot initializer with Web, H2, JPA, MariaDB dependencies.

---
+++ NobleServer/src/main/java/com/NobleScan/NobleServer/NobleServerApplication.java
@@ -0,0 +1,13 @@
+package com.NobleScan.NobleServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NobleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NobleServerApplication.class, args);
+	}
+		
+}
---
+++ NobleServer/src/test/java/com/NobleScan/NobleServer/NobleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NobleScan.NobleServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NobleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
