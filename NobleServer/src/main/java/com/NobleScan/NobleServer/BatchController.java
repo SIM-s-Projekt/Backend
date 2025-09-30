921361b4c414262421ba2b9457b67c2e099912ec
LevelWard <noah.jonsson@gmail.com>
2025-09-30 16:24:54 +0200

    Added EntityModel for batch get and changed to common root batches.

--- NobleServer/src/main/java/com/NobleScan/NobleServer/BatchController.java
+++ NobleServer/src/main/java/com/NobleScan/NobleServer/BatchController.java
@@ -1,10 +1,14 @@
 package com.NobleScan.NobleServer;
 
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class BatchController {
@@ -14,14 +18,21 @@ public class BatchController {
 		this.batchRepository = batchRepository;
 	}
 
+	// Aggregate root
+	// tag::get-aggregate-root[]
 	@GetMapping("/batches")
 	List<Batch> all(){
 		return batchRepository.findAll();
 	}
+	// end::get-aggregate-root[]
 
-	@GetMapping("/batch/{id}")
-	Batch one(@PathVariable Integer id){
-		return batchRepository.findById(id).orElseThrow(() -> new BatchNotFoundException(id));
+	@GetMapping("/batches/{id}")
+	EntityModel<Batch> one(@PathVariable Integer id){
+		Batch batch = batchRepository.findById(id).orElseThrow(() -> new BatchNotFoundException(id));
+
+		return EntityModel.of(batch, //
+				linkTo(methodOn(BatchController.class).one(id)).withSelfRel(),
+				linkTo(methodOn(BatchController.class).all()).withRel("batch"));
 	}
 
 }
